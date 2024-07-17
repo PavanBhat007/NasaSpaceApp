0d99f56ae069f5752f42bda4a8ca2f5ddefd5142
Pavan Bhat <pavanksp612@gmail.com>
2024-07-17 12:00:36 +0530

    Updates

---
+++ precache-manifest.a46fdbdd8ae8284570688ad48c875390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eea922e010c69e54e4939dca98dea49",
+    "url": "/NasaSpaceApp/index.html"
+  },
+  {
+    "revision": "dca9c770f76ec691d835",
+    "url": "/NasaSpaceApp/static/css/main.9772fbce.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/NasaSpaceApp/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/NasaSpaceApp/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "dca9c770f76ec691d835",
+    "url": "/NasaSpaceApp/static/js/main.5dda0f4b.chunk.js"
+  },
+  {
+    "revision": "f2aab5256f16b3ca3ea8",
+    "url": "/NasaSpaceApp/static/js/runtime~main.5aeef95f.js"
+  },
+  {
+    "revision": "1f7c43ba5189d9045481c59bc09cd5fa",
+    "url": "/NasaSpaceApp/static/media/bg.1f7c43ba.jpg"
+  }
+]);
